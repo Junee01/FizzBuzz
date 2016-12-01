6efb4d08e4730e7487f597c3036d32884395ea12
Sangjun Park <sangjunpark0203@gmail.com>
2016-12-01 14:59:24 +0900

    Update fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,5 @@
 #Solution 01
 
-'ruby
 (1..100).each do |i|
   if i % 3 == 0 && i % 5 == 0
     print "fizzbuzz"
@@ -12,18 +11,15 @@
     print i
   end
 end
-'
 
 #Solution 02
 
-'ruby
 (1..100).each do |i|
   tmp = ''
   tmp += 'fizz' if i % 3 == 0
   tmp += 'fizz' if i % 5 == 0
   print tmp.empty? ? i : tmp
 end
-'
 
-There are two solutions that I solved. But the principle of them is the same.
-I think there are more ideas to solve this problem but I had no idea when I got this problem on interview.
+#There are two solutions that I solved. But the principle of them is the same.
+#I think there are more ideas to solve this problem but I had no idea when I got this problem on interview.
